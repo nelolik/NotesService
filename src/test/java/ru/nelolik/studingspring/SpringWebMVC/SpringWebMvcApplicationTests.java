a4b81f8c5126cdeb4d09bff22483cc177ca00759
Aleksey <parshikovav@gamil.com>
2022-01-11 20:05:21 +0300

    Initial commit.

---
+++ src/test/java/ru/nelolik/studingspring/SpringWebMVC/SpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nelolik.studingspring.SpringWebMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
