ce9b747f0d51a6c6b24a7b2fa93eaaf8e64af0e0
Aleksey <parshikovav@gamil.com>
2022-01-15 15:37:55 +0300

    Addition and deletion of note records is done.

--- src/main/java/ru/nelolik/studingspring/SpringWebMVC/controller/UsersController.java
+++ src/main/java/ru/nelolik/studingspring/SpringWebMVC/controller/UsersController.java
@@ -3,10 +3,14 @@ package ru.nelolik.studingspring.SpringWebMVC.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.nelolik.studingspring.SpringWebMVC.db.dataset.Note;
 import ru.nelolik.studingspring.SpringWebMVC.db.dataset.User;
+import ru.nelolik.studingspring.SpringWebMVC.db.service.NotesService;
 import ru.nelolik.studingspring.SpringWebMVC.db.service.UsersService;
+import ru.nelolik.studingspring.SpringWebMVC.model.UserInput;
 
 import java.util.List;
 
@@ -15,24 +19,49 @@ import java.util.List;
 public class UsersController {
 
     private UsersService usersService;
+    private NotesService notesService;
 
     @Autowired
-    public UsersController(UsersService usersService) {
+    public UsersController(UsersService usersService, NotesService notesService) {
         this.usersService = usersService;
+        this.notesService = notesService;
     }
 
     @GetMapping()
-    public String index() {
+    public String index(Model model) {
         List<User> users = usersService.index();
-        for (User u :
-                users) {
-            System.out.println("User: " + u.toString());
-        }
+        model.addAttribute("users", users);
         return "users/index";
     }
 
     @GetMapping("/{id}")
-    public String showUser() {
+    public String showUser(@PathVariable("id") long id, Model model) {
+        User user = usersService.user(id);
+        List<Note> notes = notesService.getNotesByUserId(id);
+        model.addAttribute("user", user);
+        model.addAttribute("notes", notes);
+        model.addAttribute("input", new UserInput());
         return "users/user";
     }
+
+    @PostMapping("/{id}")
+    public String addUserNote(@ModelAttribute UserInput input,
+                              BindingResult bindingResult,
+                              @PathVariable long id) {
+        if (!bindingResult.hasErrors()) {
+            Note note = new Note(0L, id, input.getInput());
+            notesService.addNote(note);
+        } else{
+            System.out.println("Note object is null");
+        }
+        return "redirect:/users/" + id;
+    }
+
+    @GetMapping("{userId}/{noteId}")
+    public String removeUsersNote(@PathVariable("userId") long userId,
+                                    @PathVariable("noteId") long noteId) {
+        notesService.removeNote(noteId);
+        return "redirect:/users/" + userId;
+    }
 }
+
---
+++ src/main/java/ru/nelolik/studingspring/SpringWebMVC/model/UserInput.java
@@ -0,0 +1,10 @@
+package ru.nelolik.studingspring.SpringWebMVC.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInput {
+    String input;
+}
