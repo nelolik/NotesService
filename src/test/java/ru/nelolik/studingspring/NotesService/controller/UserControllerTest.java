12d5d600f583d3e89b504800092691ead7ae6f66
Aleksey <parshikovav@gamil.com>
2022-01-23 23:31:55 +0300

    Fixed UserControllerTest

--- src/test/java/ru/nelolik/studingspring/NotesService/controller/UserControllerTest.java
+++ src/test/java/ru/nelolik/studingspring/NotesService/controller/UserControllerTest.java
@@ -12,7 +12,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.shaded.org.apache.commons.lang.CharSet;
 import ru.nelolik.studingspring.NotesService.config.SpringConfig;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -78,7 +82,8 @@ public class UserControllerTest {
     public void showUserJsonTest() throws Exception {
         int id = 1;
         mockMvc.perform(get("/users/{id}/json", id)).
-                andExpect(content().contentType(MediaType.APPLICATION_JSON)).
+                andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))).
+                andExpect(content().encoding(StandardCharsets.UTF_8)).
                 andExpect(content().string(containsString("userId"))).
                 andExpect(content().string(containsString("userName"))).
                 andExpect(content().string(containsString("notes")));
@@ -87,7 +92,8 @@ public class UserControllerTest {
     @Test
     public void getAllUsersJsonTest() throws Exception {
         mockMvc.perform(get("/users")).
-                andExpect(content().contentType(MediaType.APPLICATION_JSON)).
+                andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))).
+                andExpect(content().encoding(StandardCharsets.UTF_8)).
                 andExpect(content().string(containsString("users")));
     }
 
