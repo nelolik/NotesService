74be029178507e3127a095c598f5e8ce453bd037
Aleksey <parshikovav@gamil.com>
2022-03-24 21:58:36 +0300

    Removed unnecessary annotations in DTO interfaces.

--- src/main/java/ru/nelolik/studingspring/NotesService/dto/UserToDtoConverter.java
+++ src/main/java/ru/nelolik/studingspring/NotesService/dto/UserToDtoConverter.java
@@ -2,7 +2,6 @@ package ru.nelolik.studingspring.NotesService.dto;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.springframework.stereotype.Component;
 import ru.nelolik.studingspring.NotesService.db.dataset.Role;
 import ru.nelolik.studingspring.NotesService.db.dataset.User;
@@ -12,25 +11,18 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        imports = {Role.class, UserRole.class, Collections.class})
 public interface UserToDtoConverter {
     String DEFAULT_ROLES =
-            "Collections.singletonList(new UserRole(0L, Role.ROLE_USER.name()))";
+            "java(Collections.singletonList(new UserRole(0L, Role.ROLE_USER.name())))";
 
-    @Mappings({
-            @Mapping(source = "id", target = "id"),
-            @Mapping(source = "username", target = "username")
-    })
     UserDTO userToDto(User user);
 
     List<UserDTO> usersToDto(List<User> users);
 
-    @Mappings({
-            @Mapping(source = "id", target = "id"),
-            @Mapping(source = "username", target = "username"),
-            @Mapping(target = "password", defaultValue = ""),
-            @Mapping(target = "roles", expression = DEFAULT_ROLES)
-    })
+    @Mapping(target = "password", constant = "")
+    @Mapping(target = "roles", expression = DEFAULT_ROLES)
     User userDtoToUser(UserDTO dto);
 
     List<User> userDtosToUsers(List<UserDTO> dtos);
